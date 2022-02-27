85bcd9e292a30dbbd73f3862c7ce6dba7aa8f1cd
Rishabh <rishabh.ramg@gmail.com>
2022-02-27 19:00:03 +0530

    to-do app using angular

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,30 @@
+import { Task } from "./Tasks"
+export const TASKS: Task[] = [
+  {
+    id: 1 ,
+    text: "Doctor's appointment" ,
+    day: "May 15 at 2:30 pm" ,
+    reminder: true
+  } ,
+
+  {
+    id: 2 ,
+    text: "Scrum call" ,
+    day: "May 15 at 10:30 am" ,
+    reminder: true
+  } ,
+
+  {
+    id: 3 ,
+    text: "Brown bag session" ,
+    day: "May 15 at 11:00 am" ,
+    reminder: false
+  } ,
+
+  {
+    id: 4 ,
+    text: "Doubt clearing session" ,
+    day: "May 15 at 11:00 am" ,
+    reminder: true
+  } ,
+]
